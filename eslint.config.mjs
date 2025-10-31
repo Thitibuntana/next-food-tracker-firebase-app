aba291102098f176ffbfc3a5e0482009603639cb
Thitibuntana <142291206+Thitibuntana@users.noreply.github.com>
2025-10-31 19:31:46 +0700

    W

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,12 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      "@next/next/no-img-element": "off",
+      "prefer-const": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "jsx-a11y/alt-text": "off",
+    },
   },
 ];
 
